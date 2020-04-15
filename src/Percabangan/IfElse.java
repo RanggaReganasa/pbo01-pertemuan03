8891e31fe543f1ca4f18ef3cef23ef3f5f31140c
muhammadaprizahidayat <aprizahdyt@gmail.com>
2020-04-15 16:14:47 +0800

    Percabangan IF ELSE

---
+++ src/Percabangan/IfElse.java
@@ -0,0 +1,16 @@
+package Percabangan;
+
+public class IfElse {
+    public static void main(String[] args) {
+        double totalBelanja = 25000;
+        double uangDiDompet = 53000;
+        if (uangDiDompet < totalBelanja){
+            System.out.println("Uang Kurang,Kurangi Jajan Anda");
+        }else {
+            System.out.println("Uang cukup, Selamat Menikmati !");
+        }else{
+            System.out.println("Uang pas, uwu");
+        }
+    }
+}
+
