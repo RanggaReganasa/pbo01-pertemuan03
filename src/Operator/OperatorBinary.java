9c939040fa98eb6b017d89de0b7b9f6143a3f7e4
muhammadaprizahidayat <aprizahdyt@gmail.com>
2020-04-15 15:55:07 +0800

    Binary Assigment

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package Operator;
+
+public class OperatorBinary {
+
+    //Operator (=) sebagai assigment/penugasan
+    //Menempatkan nilai dalam variable
+    //Baris 5-7 adalah comment/komentar
+
+    String nama = "Rangga Reganansa";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+}
